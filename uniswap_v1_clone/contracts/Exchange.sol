41c5b463356b0ac883a72f91ee436cf81f968d35
komus-Israel <komolehinisrael@gmail.com>
2022-05-12 20:21:51 +0100

    installed chai; added a dev comment to the exchange contract file to brief what the exchange contract is about

---
+++ uniswap_v1_clone/contracts/Exchange.sol
@@ -0,0 +1,15 @@
+pragma solidity 0.8.13;
+
+/// @title  Exchange
+
+/**
+    *   @dev    an exchange contract to be deployed for each token pair. e.g ETH/DAI
+    The factory contract will keep track of every deployed exchange contract
+ */
+
+
+contract Exchange {
+
+
+
+}
