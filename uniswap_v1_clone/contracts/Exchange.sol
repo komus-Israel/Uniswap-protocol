ec093a60b273f65ef68b66a1d8c8f9ec63b4d7cc
komus-Israel <komolehinisrael@gmail.com>
2022-05-12 20:27:56 +0100

    the address for the token for the exchange is initialized in the constructor

--- uniswap_v1_clone/contracts/Exchange.sol
+++ uniswap_v1_clone/contracts/Exchange.sol
@@ -1,15 +1,32 @@
 pragma solidity 0.8.13;
+import "./utils/IERC20.sol";
 
 /// @title  Exchange
 
 /**
     *   @dev    an exchange contract to be deployed for each token pair. e.g ETH/DAI
-    The factory contract will keep track of every deployed exchange contract
- */
+    The factory contract will keep track of every deployed exchange contract.
+
+    *   This exchange contract will illustrate the uniswap v1 clone. So the swap will
+        strictly be between an ERC20 token and Ether
+
+    *   The token address for the exchange will be initailized in the constructor
+*/
 
 
 contract Exchange {
 
+    address private tokenAddress;
 
+    constructor (address _tokenAddress) {
+
+        tokenAddress = _tokenAddress;
+
+    }
+
+
+    function getTokenAddress () external view returns (address) {
+        return tokenAddress;
+    }
 
 }
